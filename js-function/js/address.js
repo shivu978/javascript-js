444c2413acbc2e4f6e82cd6cde64b7e55d1a6995
shivu978 <131174532+shivu978@users.noreply.github.com>
2023-11-30 13:28:20 +0530

    Add files via upload

---
+++ js-function/js/address.js
@@ -0,0 +1,35 @@
+
+function address()
+
+{
+
+   var add= " Viththal Apartment<br>Race Course Rd<br> Jagnath Plot<br> Rajkot<br>Gujarat 360001"
+ document.write(add);
+}
+
+
+    function address1()
+
+    {
+
+    var add="Shop No 6<br> Kilol Complex<br> Amin Marg<br> Rajkot<br> Gujarat 360001"
+    document.write(add);
+    }
+
+
+function address2()
+
+{
+
+   var add= "B/5, Kld Road<br> Panchnath Camp<br> Opposite St Mary School<br> Rajkot<br> Gujarat 360001"
+ document.write(add);
+}
+
+
+function address3()
+
+{
+
+   var add= "Sapphire<br> Nana Mava Main Rd<br> Opposite To Satyam Party Plot<br> Chandra Park<br> Rajkot<br> Gujarat 360005"
+ document.write(add);
+}
